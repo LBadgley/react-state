33a9051c14df228a0d2d032b970f819f0f2e511e
Laura Badgley <l.badgleyy@gmail.com>
2019-05-14 11:09:09 -0700

    stying buttons

---
+++ src/components/ColorPicker.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './ColorPicker.css';
+
+export default function ColorPicker() {
+  const red = () => {
+    console.log('red');
+  };
+
+  const yellow = () => {
+    console.log('yellow');
+  };
+
+  const blue = () => {
+    console.log('blue');
+  };
+  return (
+    <section className={styles.ColorPicker}>
+      <button className={styles.red} onClick={red}>Red</button>
+      <button className={styles.yellow} onClick={yellow}>Yellow</button>
+      <button className={styles.blue} onClick={blue}>Blue</button>
+    </section>
+  );
+}
