e8afacadbe258e0e52228ae062c79558c008e9e0
Laura Badgley <l.badgleyy@gmail.com>
2019-05-14 13:11:16 -0700

    div rendering and changing color

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import styles from './colorPicker.css';
 
-export default function ColorPicker() {
-  const red = () => {
-    console.log('red');
+export default class ColorPicker extends PureComponent {
+  state = {
+    color: ''
   };
 
-  const yellow = () => {
-    console.log('yellow');
-  };
+  selectedColor = color => {
+    this.setState({ color });
+  }
 
-  const blue = () => {
-    console.log('blue');
-  };
-  return (
-    <section className={styles.ColorPicker}>
-      <button className={styles.red} onClick={red}>Red</button>
-      <button className={styles.yellow} onClick={yellow}>Yellow</button>
-      <button className={styles.blue} onClick={blue}>Blue</button>
-      {/* <div className={styles[color]}></div> */}
-    </section>
-  
-  );
+  render() {
+    const { color } = this.state;
+
+    return (
+      <section className={styles.ColorPicker}>
+        <button className={styles.red} onClick={this.selectedColor.bind(null, 'red')}>Red</button>
+        <button className={styles.yellow} onClick={() => this.selectedColor('yellow')}>Yellow</button>
+        <button className={styles.blue} onClick={() => this.selectedColor('blue')}>Blue</button>
+        <div className={styles[color]}>ayyeeee</div>
+      </section>
+    );
+  }
 }
