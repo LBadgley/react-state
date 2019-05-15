d0f13bd104411de49e527003207d9a3db77ec78a
Laura Badgley <l.badgleyy@gmail.com>
2019-05-14 17:34:30 -0700

    color display test passing

--- src/components/ColorDisplay.test.js
+++ src/components/ColorDisplay.test.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import ColorDisplay from './ColorDisplay';
+
+describe('color display test', () => {
+  it('displays the color', () => {
+    const wrapper = shallow(<ColorDisplay color="blue"/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
