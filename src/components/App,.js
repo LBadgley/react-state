1ca8d2fe3e2443ca73bc8c081da73e02430efa91
Laura Badgley <l.badgleyy@gmail.com>
2019-05-15 17:36:27 -0700

    randomColor test passing

--- src/components/App,.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import RandomColor from './RandomColor.js';
-
-export default function App() {
-  return <RandomColor />;
-}
--- src/components/RandomColor.js
+++ src/components/RandomColor.js
@@ -1,18 +1,40 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import styles from './RandomColor.css';
+// import PropTypes from 'prop-types';
+// import styles from './RandomColor.css';
 
 export default class RandomColor extends PureComponent {
   state = {
     color: '',
     trigger: new Date(),
     img: ''
-  };
-
+  }
   randomColor = () => {
     const colors = ['darkRed', 'indigo', 'mediumBlue', 'dodgerBlue', 'gold', 'orangeRed'];
-    const randomColors = Math.floor(Math.random() * colors.length);
+    const getRandom = Math.floor(Math.random() * colors.length);
 
-    return colors[randomColors];
+    return colors[getRandom];
+  }
+
+  componentDidMount() {
+    this.myInterval = setInterval(() => {
+      this.setState({ color: this.getRandom(), trigger: new Date(), img: '' });
+    }, 1500);
+  }
+
+  componentDidUpdate(prevState) {
+    if(prevState.color === this.state.color) {
+      this.setState({ img: 'https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg' });
+    }
+  }
+  componentWillUnmount() {
+    this.myInterval && clearInterval(this.myInterval);
+  }
+
+  render() {
+    const { color, img } = this.state;
+    const styles = {
+      background: img ? `center / contain url (${img})` : color,
+    };
+    return <div className={styles[this.state.color]}></div>;
   }
 }
